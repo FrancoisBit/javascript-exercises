f2b3b5f9234805025ccd16b15a700b9ae94b299b
FRANCOIS BIT <bit.francois@gmail.com>
2022-07-16 22:00:15 +0300

    Fix repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,21 @@
-const repeatString = function() {
+const repeatString = function(string, numberOfTime) {
+    if(numberOfTime > 0) {
+        let repeatString = string;
 
+        for(let i = 1 ; i < numberOfTime; i++) {
+            repeatString = repeatString.concat(string);
+        }
+
+        return repeatString;
+    }
+
+    else if(numberOfTime === 0) {
+        return "";
+    }
+
+    else {
+        return "ERROR"
+    }
 };
 
 // Do not edit below this line

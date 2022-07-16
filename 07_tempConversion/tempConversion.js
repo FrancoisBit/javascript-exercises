584c10b924731de07a7a15e6984eecba0e255456
FRANCOIS BIT <bit.francois@gmail.com>
2022-07-16 22:44:11 +0300

    Fix tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
+const ftoc = function(temperatureF) {
+    let temperatureC = (temperatureF - 32) / 1.8;
+    temperatureC = Math.round(temperatureC * 10) / 10;
 
+    return temperatureC;
 };
 
-const ctof = function() {
-
+const ctof = function(temperatureC) {
+    let temperatureF = (temperatureC * 1.8) + 32;
+    temperatureF = Math.round(temperatureF * 10) / 10;
+    
+    return temperatureF;
 };
 
 // Do not edit below this line

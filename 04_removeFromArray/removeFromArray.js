ce977ce76fecbb408cc9f1b8c9c02099c5480c03
FRANCOIS BIT <bit.francois@gmail.com>
2022-07-16 22:43:27 +0300

    Fix removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    args.forEach((itemArgs) => {
+        array.forEach((itemArray, index) => {
+            if(itemArgs === itemArray) {
+                array.splice(index, 1);
+            }
+        })
+    })
 
+    return array;
 };
 
 // Do not edit below this line
